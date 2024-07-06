8d0a018daea264239f09f48cef80f1853d2484af
Aung Kaung Myat <akm292516@gmail.com>
2024-07-06 15:56:27 +0800

    Initial Commit

---
+++ 2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024
+//
+//  Created by Aung Kaung Myat on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024/_024App.swift
@@ -0,0 +1,17 @@
+//
+//  _024App.swift
+//  2024
+//
+//  Created by Aung Kaung Myat on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct _024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
